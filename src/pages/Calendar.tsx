b7a3b2014437d47f80ee5517eb724f17da560571
Jiali Zhang <zhangjiali86@gmail.com>
2023-04-28 12:00:51 +0200

    custom calendar

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,28 +1,41 @@
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import './index.css'; 
 
-// type events = {
-//     start: Date,
-//     end: Date,
-//     title: string,
-// }
-
-const events = {
-    start: moment('2023-04-27T10:00:00').toDate(),
-    end: moment('2023-04-27T12:00:00').toDate(),
+const events = [
+    {
+    start: moment("2023-04-27T12:00:00").toDate(),
+    end: moment("2023-04-27T15:00:00").toDate(),
     title: "Try",
-}
+    },
+    {
+    start: moment("2023-04-28T12:00:00").toDate(),
+    end: moment("2023-04-28T13:00:00").toDate(),
+    title: "Try2",
+    },
+    {
+    start: moment("2023-04-28T12:00:00").toDate(),
+    end: moment("2023-04-28T15:00:00").toDate(),
+    title: "Try4",
+    },
+    {
+    start: moment("2023-04-29T12:00:00").toDate(),
+    end: moment("2023-04-29T13:00:00").toDate(),
+    title: "Try3",
+    },
+]
 
 function Calendar() {
     const localizer = momentLocalizer(moment)
     
   return (
-    <div className="h-screen">
+    <div className="h-screen m-5">
           <BigCalendar
       localizer={localizer}
-    //   events={events}
-      startAccessor="start"
-      endAccessor="end"
+      events={events}
+    //   startAccessor="start"
+    //   endAccessor="end"
+    // toolbar={false}
     />
     </div>
   )
