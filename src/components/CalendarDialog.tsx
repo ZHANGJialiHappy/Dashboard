c8a227df50fff6e746e5d498c5f89152a4b15e0a
Jiali Zhang <zhangjiali86@gmail.com>
2023-04-29 22:34:26 +0200

    add card style

--- src/components/CalendarDialog.tsx
+++ src/components/CalendarDialog.tsx
@@ -6,12 +6,12 @@ function CalendarDialog() {
     <div className="flex flex-wrap justify-center gap-3 items-center">
 
       <div className="card rounded-s-lg bg-base-100 shadow-xl">
-        <div className="h-5 bg-gray-100 rounded-t-lg opacity-75 flex justify-end p-1"> 
+        <div className="h-8 bg-gray-200 rounded-t-lg opacity-75 flex justify-end p-2 text-lg"> 
         <TiDeleteOutline/>
         </div>
-        <div className="card-body justify-venter">
-        <input type="text" placeholder="Add title and time " className="input input-bordered rounded-none w-full border-0 border-b-2 mx-0.5 p-0" />
-
+        <div className="card-body mt-1">
+        <input type="text" placeholder="Add title" className="input input-bordered rounded-none w-full text-lg border-0 border-b-2 h-5" />
+        <textarea className="textarea textarea-bordered" placeholder="Add description"></textarea>
         </div>
       </div>
   </div>
